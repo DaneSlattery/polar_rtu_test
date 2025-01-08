4f8b6b87b1b6dfdc9a243c1974760de931806301
Dane Slattery <dane@polarmonitoring.com>
2025-01-08 16:50:56 +0200

    Set all inputs

--- src/rtu_test/cli.py
+++ src/rtu_test/cli.py
@@ -2,82 +2,157 @@
 #
 # SPDX-License-Identifier: MIT
 import click
-
+import asyncio
 from rtu_test.__about__ import __version__
-from rtu_test.modbus import connect, read_input_1, set_output_1, enumerature_tty
-
+# from rtu_test.modbus import connect, read_input_1, set_output, enumerature_tty
+import rtu_test.modbus as modbus
 
 @click.command()
 @click.help_option(*("--help", "-h"))
 @click.version_option(__version__, "-V", "--version")
 def rtu_test():
     click.echo(f"RTU Tester  ")
+    asyncio.run(start_menu())
+    # menu = "main"
 
-    menu = "main"
-
-    while True:
-        if menu == "main":
-            click.echo("Main Menu:")
-            click.echo("    s: start")
-            click.echo("    l: list ports")
-            click.echo("    q: quit")
-            char = click.getchar()
-            if char == "s":
-                menu = "start"
-            elif char == "q":
-                menu = "quit"
-            elif char == "l":
-                menu = "list"
-            else:
-                click.echo("Invalid input")
-        elif menu == "start":
-            menu = start_menu()
-        elif menu == "list":
-            click.echo("List serial ports")
-            ports = enumerature_tty()
-            click.echo(len(ports))
-            for port in ports:
-                click.echo(port)
-            click.echo("    b: back")
-            char = click.getchar()
-            if char == "b":
-                menu = "main"
-            else:
-                click.echo("Invalid input")
-        elif menu == "quit":
-            return
+    # while True:
+    #     if menu == "main":
+    #         click.echo("Main Menu:")
+    #         click.echo("    s: start")
+    #         click.echo("    l: list ports")
+    #         click.echo("    q: quit")
+    #         char = click.getchar()
+    #         if char == "s":
+    #             menu = "start"
+    #         elif char == "q":
+    #             menu = "quit"
+    #         elif char == "l":
+    #             menu = "list"
+    #         else:
+    #             click.echo("Invalid input")
+    #     elif menu == "start":
+    #         menu = start_menu()
+    #     elif menu == "list":
+    #         click.echo("List serial ports")
+    #         ports = enumerature_tty()
+    #         click.echo(len(ports))
+    #         for port in ports:
+    #             click.echo(port)
+    #         click.echo("    b: back")
+    #         char = click.getchar()
+    #         if char == "b":
+    #             menu = "main"
+    #         else:
+    #             click.echo("Invalid input")
+    #     elif menu == "quit":
+    #         return
 
 
-def start_menu():
+async def start_menu():
 
     click.echo("Start RTU Test")
+
     is_connected = click.confirm("Is the RTU connected and powered?")
 
     if not is_connected:
         click.echo("Connect the RTU")
-        return "start"
+        exit(1)
 
-    click.echo("Running Modbus Checks")
+    try:
+        client =await modbus.connect("/dev/ttyUSB0")
+    except Exception:
+        click.echo("RS485 USB Failed to connect.")
+        exit(1)
 
-    click.echo("Pulse Out 1")
-    click.echo("Pulse Out 2")
-    click.echo("Pulse Out 3")
-    click.echo("Pulse Out 4")
 
-    click.echo("Set Out 1")
-    click.echo("Set Out 2")
-    click.echo("Set Out 3")
-    click.echo("Set Out 4")
+    click.echo("1. Print All")
+    await print_all(client)
 
-    click.confirm("Disconnect RTU power")
-    click.echo("Polling RTU until dead")
-    click.echo("RTU Dead")
-    click.confirm("Reconnect RTU power")
+    if not click.confirm("Proceed?"):
+        exit(1)
+    
+    click.echo("2. Set All Outputs High")
+    result =await modbus.set_digi_output(client,modbus.GPIO.ALL,0xffff)
+    if result.isError():
+        click.echo(f"Failed to set all high, result={result}")
+        exit(1)
 
-    click.echo("Check Out 1")
-    click.echo("Check Out 2")
-    click.echo("Check Out 3")
-    click.echo("Check Out 4")
+    if not click.confirm("All outputs high?"):
+        click.echo("Fail, output not high")
 
-    click.echo("All outputs remembered. Test Pass.")
-    return "main"
+        exit(1)
+    
+    click.echo("3. Set All Inputs High")
+    click.pause("Hit any key when all inputs are set high.")
+    result = await modbus.get_digi_input(client,modbus.GPIO.ALL)
+    if result != 0b0000_0000_0000_1111:
+        click.echo("Fail, input not high")
+        exit(1)
+
+    # set_output(client, 1)
+    # click.echo("Pulse Out 2")
+    # click.echo("Pulse Out 3")
+    # click.echo("Pulse Out 4")
+    # click.confirm("Did all outputs pulse?")
+
+    # click.echo("Set Out 1")
+    # click.echo("Set Out 2")
+    # click.echo("Set Out 3")
+    # click.echo("Set Out 4")
+
+    # click.confirm("Did all outputs get set?")
+
+    # click.confirm("Switch all inputs to 1")
+
+    # click.echo("Check In 1")
+    # click.echo("Check In 2")
+    # click.echo("Check In 3")
+    # click.echo("Check In 4")
+
+    # click.confirm("Pulse Each PC ")
+
+    # click.echo("Check PC 1")
+    # click.echo("Check PC 2")
+
+    # click.confirm("Disconnect RTU power")
+    # click.echo("Polling RTU until dead")
+    # click.echo("RTU Dead")
+    # click.confirm("Reconnect RTU power")
+
+    # click.echo("Check Out 1")
+    # click.echo("Check Out 2")
+    # click.echo("Check Out 3")
+    # click.echo("Check Out 4")
+
+    # click.echo("All outputs remembered. Test Pass.")
+    exit(0)
+
+
+async def print_all(client:modbus.AsyncModbusSerialClient):
+    digi_out =await  modbus.get_digi_output(client,modbus.GPIO.ALL)
+    click.echo(f"Out = \t{digi_out:010b}")
+    
+    out_settings = []
+
+    for dig_out in [modbus.GPIO.IO1,modbus.GPIO.IO2,modbus.GPIO.IO3,modbus.GPIO.IO4,]:
+        setting = await modbus.get_digi_output_config(client,dig_out)
+        click.echo(f"Out Conf {dig_out} = \t{setting}")
+        out_settings.append(setting)
+
+    digi_in =await modbus.get_digi_input(client,modbus.GPIO.ALL)
+    click.echo(f"In = \t{digi_in:010b}")
+
+    analogs = []
+    for a in modbus.AnalogIn:
+        analog = await modbus.get_analog_input(client,a)
+        click.echo(f"Analog {a} = \t{analog}")
+        analogs.append(analog)
+
+
+    pcs = []
+    for pc in modbus.PulseCounter:
+        count =await modbus.get_pulse_count(client,pc)
+        click.echo(f"PC {pc} = \t{count}")
+        pcs.append(count)
+
+    
--- src/rtu_test/modbus.py
+++ src/rtu_test/modbus.py
@@ -1,29 +1,94 @@
-from pymodbus.client import ModbusSerialClient
+from pymodbus.client import AsyncModbusSerialClient
+from pymodbus import FramerType
+
+from enum import Enum
 import serial
 import serial.tools
 import serial.tools.list_ports
 # from pymodbus.client.serial import
 
 
+PC_REGISTER_OFFSET = 300
+ANALOG_REGISTER_OFFSET = 400
+IN_REGISTER_OFFSET = 500
+OUT_REGISTER_OFFSET = 600
+PULSE_OUT_REGISTER_OFFSET = (700 -1) # no all option
+OUT_CONFIG_REGISTER_OFFSET = 650 -1 #no all option
+
 def enumerature_tty():
     ports = serial.tools.list_ports.comports()
     return ports
 
 
-def connect(tty) -> ModbusSerialClient:
-    client = ModbusSerialClient(tty, baudrate=9600)
+async def connect(tty) -> AsyncModbusSerialClient:
+
+    client = AsyncModbusSerialClient(tty, baudrate=9600,framer=FramerType.RTU)
+    connected = await client.connect()
+    if not connected:
+        raise ConnectionError(f"{tty} failed to connect")
     return client
 
 
-def set_output_1(client: ModbusSerialClient, value):
-    result = client.write_register(1000, value)
+class GPIO(Enum):
+    ALL = 0
+    IO1 = 1
+    IO2 = 2
+    IO3 = 3
+    IO4 = 4
 
-    print(result.isError())
+class AnalogIn(Enum):
+    IO1 = 0
+    IO2 = 1
+    IO3 = 2
+    IO4 = 3
+
+class PulseCounter(Enum):
+    IO1 = 0
+    IO2 = 1
+    
+class DigOutDefaultState(Enum):
+    OFF=0
+    ON = 1
+    REMEMBER=2
 
 
-def read_input_1(client: ModbusSerialClient) -> int:
-    result = client.read_holding_registers(200)
+async def set_digi_output(client:AsyncModbusSerialClient, output_no:GPIO, value=1):
+    result = await client.write_register(
+        OUT_REGISTER_OFFSET + output_no.value, value)
 
-    print(result)
+    return result
 
+async def set_digi_output_config(client: AsyncModbusSerialClient, output_no:GPIO, value: DigOutDefaultState= DigOutDefaultState.REMEMBER):
+    if output_no == GPIO.ALL:
+        raise Exception("All not a valid option")
+    
+    result =await client.write_register(
+        OUT_CONFIG_REGISTER_OFFSET + output_no.value, value)
+
+    return result.isError()
+
+
+async def get_digi_output_config(client:AsyncModbusSerialClient, output_no:GPIO):
+    if output_no == GPIO.ALL:
+        raise Exception("All not a valid option")
+    registers= (await client.read_holding_registers(OUT_CONFIG_REGISTER_OFFSET+output_no.value)).registers
+    return DigOutDefaultState(registers[0])
+
+
+async def get_digi_output(client:AsyncModbusSerialClient, output_no:GPIO):
+    return  (await client.read_holding_registers(OUT_REGISTER_OFFSET+output_no.value)).registers[0]
+
+async def get_analog_input(client:AsyncModbusSerialClient, input_no:AnalogIn):
+    register = (await client.read_holding_registers(ANALOG_REGISTER_OFFSET+input_no.value)).registers[0]
+    grad = (20.0/20480.0)
+    offset = 4
+    return register* grad + offset
+
+async def get_digi_input(client: AsyncModbusSerialClient, input_no: GPIO):
+    result =await client.read_holding_registers(IN_REGISTER_OFFSET + input_no.value)
     return result.registers[0]
+
+async def get_pulse_count(client: AsyncModbusSerialClient, input_no: PulseCounter):
+    result =await  client.read_holding_registers(IN_REGISTER_OFFSET + input_no.value)
+    return result.registers[0]
+
